52671c1bdee079505f1b6596f9b3ea313f4d46b9
Antonio Cuadros <49162117+Acuadros95@users.noreply.github.com>
2022-10-25 08:07:33 +0200

    Ignore test_msgs (#99)
    Signed-off-by: acuadros95 <acuadros1995@gmail.com>
    
    Signed-off-by: acuadros95 <acuadros1995@gmail.com>
    (cherry picked from commit 2cfb9028967ef575c50d596e5e5ba5e0d5a80886)
    
    # Conflicts:
    #	modules/libmicroros/libmicroros.mk

--- modules/libmicroros/libmicroros.mk
+++ modules/libmicroros/libmicroros.mk
@@ -93,8 +93,13 @@ $(COMPONENT_PATH)/micro_ros_src/src:
     touch src/common_interfaces/actionlib_msgs/COLCON_IGNORE; \
 	touch src/common_interfaces/std_srvs/COLCON_IGNORE; \
 	touch src/rcl/rcl_yaml_param_parser/COLCON_IGNORE; \
+<<<<<<< HEAD
 	touch src/rcl_logging/rcl_logging_log4cxx/COLCON_IGNORE; \
     touch src/rcl_logging/rcl_logging_spdlog/COLCON_IGNORE;
+=======
+    touch src/rcl_logging/rcl_logging_spdlog/COLCON_IGNORE; \
+    touch src/rcl_interfaces/test_msgs/COLCON_IGNORE;
+>>>>>>> 2cfb902 (Ignore test_msgs (#99))
 
 $(COMPONENT_PATH)/micro_ros_src/install: configure_colcon_meta configure_toolchain $(COMPONENT_PATH)/micro_ros_dev/install $(COMPONENT_PATH)/micro_ros_src/src
 	cd $(UROS_DIR); \
